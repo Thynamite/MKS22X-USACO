5af48a7d9f70075e8fe0261e755d2c234e00bee6
Brandon Chao <bchao1@stuy.edu>
2019-03-05 21:46:18 -0500

    method to return coordinate of highest point

--- USACO.java
+++ USACO.java
@@ -16,14 +16,14 @@ public class USACO{
   }
 
 
-  public class bronzeSolve{
+  public class bronzeSolve {
 
     int[][] field;
-    ArrayList<int> firstLine;
-    public bronzeSolve(File f) {
+    ArrayList<Integer> firstLine;
+    public bronzeSolve(File f) throws FileNotFoundException {
       Scanner s = new Scanner(f);
       String first = s.nextLine();
-      for (int x = 0; x < first.length; x++) {
+      for (int x = 0; x < first.length(); x++) {
         //put firstline into firstline
       }
       //make an array to dimensions given
@@ -31,7 +31,7 @@ public class USACO{
     }
 
     private void stampDown(int row, int col, int inches) {
-      ArrayList<int> fieldAffect;  //holds the coordinates of all the plots to be affected
+      ArrayList<Integer> fieldAffect = new ArrayList<Integer>();  //holds the coordinates of all the plots to be affected
       for (int x = 0; x < 3; x++) {
         for (int y = 0; y < 3; y++) {
           if (row + x < field.length && col + y < field[row].length) {
@@ -40,7 +40,26 @@ public class USACO{
           }
         }
       }
-      
+
+    }
+
+    private void lower(int row, int col) {
+      field[row][col]--;
+    }
+
+    private int[] findHighest(ArrayList<Integer> help) {
+      int[] me = new int[2];
+      int r = help.get(0);
+      int c = help.get(1);
+      for (int x = 2; x < help.size(); x+=2) {
+        if (field[help.get(x)][help.get(x+1)] > field[r][c]){
+          r = help.get(x);
+          c = help.get(x+1);
+        }
+      }
+      me[0] = r;
+      me[1] = c;
+      return me;
     }
   }
 }
