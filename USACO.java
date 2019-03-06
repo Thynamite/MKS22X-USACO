5ccbad464e75257c3dfb6943b102789d1f7c71d8
Brandon Chao <bchao1@stuy.edu>
2019-03-05 21:12:55 -0500

    preliminary skeleton of potential for bronze

---
+++ USACO.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class USACO{
+
+  public static int bronze(String filename){
+    try {
+      File f = new File(filename);
+    }
+  }
+
+  private int bronzeH(File filename) {
+
+  }
+  private void stampDown(int row, int col, int inches) {
+
+  }
+}
